c9ed8d543715a2eb2e9b09d41bcc22619c58dc63
Artem <super.alshan@mail.ru>
2022-06-16 12:28:52 +0300

    Added a switch to change the dark theme to a light one and vice versa.Added local storage to save the user's choice of dark or light theme.

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -2,6 +2,8 @@
 jQuery('.send_data').submit(function(){
 	jQuery('.content').css('visibility', 'hidden');
 	jQuery('.content').fadeOut('slow')
+	jQuery('#dark_theme').css('visibility', 'hidden');
+	jQuery('#light_theme').css('visibility', 'hidden');
 	jQuery('html').css('cursor', 'wait');
 	
 	setTimeout(function(){
@@ -10,4 +12,57 @@ jQuery('.send_data').submit(function(){
 		jQuery('#loading').fadeIn(900)
 	}, 1500);
 	
+	
 });
+
+
+
+jQuery('#dark_theme').click(function(){
+	var dark_theme = document.documentElement.style.cssText = "--blueviolet: black";
+	
+	localStorage.setItem('bg', 'night')
+	
+	var dark_theme = document.documentElement.style.cssText = "--blueviolet: black";
+	
+	jQuery('.crescent_moon').removeClass('filter-icons-black');
+	jQuery('.crescent_moon').addClass('filter-icons-white');
+	
+	jQuery('#light_theme').removeClass('active')
+	jQuery('#dark_theme').addClass('active')
+		
+});
+
+if (localStorage.getItem('bg') == 'night'){
+	
+	var dark_theme = document.documentElement.style.cssText = "--blueviolet: black";
+	
+	jQuery('.crescent_moon').removeClass('filter-icons-black');
+	jQuery('.crescent_moon').addClass('filter-icons-white');
+	
+	jQuery('#light_theme').removeClass('active')
+	jQuery('#dark_theme').addClass('active')
+}
+
+jQuery('#light_theme').click(function(){
+	
+	localStorage.setItem('bg', 'light')
+	
+	document.documentElement.style.cssText = "--blueviolet: blueviolet";
+	
+	jQuery('#light_theme').addClass('active')
+	jQuery('#dark_theme').removeClass('active')
+	
+	jQuery('.crescent_moon').addClass('filter-icons-black');
+	jQuery('.crescent_moon').removeClass('filter-icons-white');
+});
+
+if (localStorage.getItem('bg') == 'light'){
+	
+	document.documentElement.style.cssText = "--blueviolet: blueviolet";
+	
+	jQuery('#light_theme').addClass('active')
+	jQuery('#dark_theme').removeClass('active')
+	
+	jQuery('.crescent_moon').addClass('filter-icons-black');
+	jQuery('.crescent_moon').removeClass('filter-icons-white');
+}
